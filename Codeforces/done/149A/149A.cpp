14940da9827837c44a556b88f95e40ae7a6d9424
callistusystan <callistusystan@gmail.com>
2017-10-25 03:52:30 +1100

    solved first page of Codeforces problems (99 problems)

---
+++ Codeforces/done/149A/149A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+
+  int K; cin >> K;
+  vi A(12);
+  for (int i=0;i<12;i++) cin >> A[i];
+  sort(A.begin(), A.end());
+  reverse(A.begin(), A.end());
+  int i, G = 0;
+  for (i=0;i<12;i++) {
+    if (G >= K) break;
+    G += A[i];
+  }
+
+  cout << i << endl;
+
+  return 0;
+}
